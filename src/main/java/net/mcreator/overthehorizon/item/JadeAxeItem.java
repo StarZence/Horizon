78d6aa3ade4a5cb260c74b56eb506ee83aae5cac
StarZence <636603@S204274.student.trinity.vic.edu.au>
2021-02-09 13:07:48 +1100

    Added Jade Axe

---
+++ src/main/java/net/mcreator/overthehorizon/item/JadeAxeItem.java
@@ -0,0 +1,63 @@
+
+package net.mcreator.overthehorizon.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.AxeItem;
+import net.minecraft.client.util.ITooltipFlag;
+
+import net.mcreator.overthehorizon.OverthehorizonModElements;
+
+import java.util.List;
+
+@OverthehorizonModElements.ModElement.Tag
+public class JadeAxeItem extends OverthehorizonModElements.ModElement {
+	@ObjectHolder("overthehorizon:jade_axe")
+	public static final Item block = null;
+	public JadeAxeItem(OverthehorizonModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new AxeItem(new IItemTier() {
+			public int getMaxUses() {
+				return 25;
+			}
+
+			public float getEfficiency() {
+				return 4f;
+			}
+
+			public float getAttackDamage() {
+				return 8f;
+			}
+
+			public int getHarvestLevel() {
+				return 1;
+			}
+
+			public int getEnchantability() {
+				return 0;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.EMPTY;
+			}
+		}, 1, -2.5f, new Item.Properties().group(ItemGroup.TOOLS)) {
+			@Override
+			public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
+				super.addInformation(itemstack, world, list, flag);
+				list.add(new StringTextComponent("THis was once Gunther's weapon in the great war"));
+			}
+		}.setRegistryName("jade_axe"));
+	}
+}
